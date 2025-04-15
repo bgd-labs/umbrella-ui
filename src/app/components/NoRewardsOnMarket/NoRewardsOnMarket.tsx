19e9e28a38afa6d62d3e934774fa4b6ebea2b853
Aleksei <gorbunovalexeyleti@gmail.com>
2025-04-15 15:26:17 +0200

    fix: update NoRewardsOnMarket message for clarity

--- src/app/components/NoRewardsOnMarket/NoRewardsOnMarket.tsx
+++ src/app/components/NoRewardsOnMarket/NoRewardsOnMarket.tsx
@@ -5,7 +5,7 @@ export const NoRewardsOnMarket = () => {
     <div className="flex flex-col items-center justify-center gap-14">
       <NoRewardsOnMarketIcon />
       <div className="max-w-[440px] text-center text-xl">
-        At the moment, umbrella rewards are not offered on any assets in the chosen market
+        At the moment, there is no asset with rewards on Umbrella in this pool
       </div>
     </div>
   );
