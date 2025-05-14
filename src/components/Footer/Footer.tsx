143532189abd8e5e25cecf490559cf77b8cfa9ae
Aleksei <gorbunovalexeyleti@gmail.com>
2025-05-14 13:36:17 +0200

    feat: replace umbrella logo with new BGD logo and update footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,15 @@
+import BGDLogoDarkIcon from "../../../public/images/BGD-logo-dark.svg";
 import BGDLogoIcon from "../../../public/images/BGD-logo.svg";
-import TwitterIcon from "../../../public/images/twitter.svg";
 import GithubIcon from "../../../public/images/github.svg";
+import TwitterIcon from "../../../public/images/twitter.svg";
 import WebIcon from "../../../public/images/web.svg";
 
 export const Footer = () => {
   return (
     <footer className="mt-5 flex items-center justify-center pt-5 pb-10 md:mt-10">
-      <div className="flex items-center gap-3 rounded-xl border-2 px-3 py-1">
-        <BGDLogoIcon className="h-[33px] w-[34px] dark:text-white" />
+      <div className="dark:border-main-400 flex items-center gap-3 rounded-xl border-2 px-3 py-1">
+        <BGDLogoIcon className="h-[33px] w-[34px] dark:hidden" />
+        <BGDLogoDarkIcon className="hidden h-[33px] w-[34px] dark:block dark:text-white" />
 
         <div className="flex flex-col">
           <div className="text-sm font-semibold">by BGD Labs</div>
