1fe40777ec6e8c10780a8e6be88fb0b1986ae940
Aleksei <gorbunovalexeyleti@gmail.com>
2025-05-26 21:05:51 +0200

    feat: enhance Checkbox component with focus styles and check icon

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { CheckIcon } from "lucide-react";
 import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from "react";
 
 export const Checkbox = forwardRef<
@@ -9,7 +10,8 @@ export const Checkbox = forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "border-main-950 size-4 cursor-pointer border border-t-[3px] border-r-[3px] dark:border-white",
+      "border-main-950 size-4 cursor-pointer border border-t-[3px] border-r-[3px] outline-none dark:border-white",
+      "focus:bg-secondary-500 data-[state=checked]:bg-secondary-500 focus:border-1 data-[state=checked]:border-1",
       className,
     )}
     {...props}
@@ -23,10 +25,8 @@ export const CheckboxIndicator = forwardRef<
   ComponentRef<typeof CheckboxPrimitive.Indicator>,
   ComponentPropsWithoutRef<typeof CheckboxPrimitive.Indicator>
 >(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Indicator
-    ref={ref}
-    className={cn("bg-main-950 ml-0.5 block size-2 dark:bg-white", className)}
-    {...props}
-  />
+  <CheckboxPrimitive.Indicator ref={ref} className={className} {...props}>
+    <CheckIcon className="dark:text-main-950 size-full" />
+  </CheckboxPrimitive.Indicator>
 ));
 CheckboxIndicator.displayName = CheckboxPrimitive.Indicator.displayName;
