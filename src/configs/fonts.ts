b2dd2017ffbae63183cd40fa5cd4ac3493c0064e
K.K. <m@mnm.mn>
2025-06-02 19:34:47 +0900

    chore: remove unused fonts

--- src/configs/fonts.ts
+++ src/configs/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Space_Grotesk, Silkscreen } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
@@ -9,10 +9,4 @@ export const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-heading",
   weight: ["700"],
-});
-
-export const silkscreen = Silkscreen({
-  subsets: ["latin"],
-  variable: "--font-countdown",
-  weight: ["400"],
 });
