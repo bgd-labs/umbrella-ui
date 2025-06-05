8c5203100a87a6f5ab47d16ff1cae5b06d92343f
K.K. <m@mnm.mn>
2025-06-05 10:09:29 +0900

    feat: add a tooltip

--- src/app/components/StakedSummary/StakedSummary.tsx
+++ src/app/components/StakedSummary/StakedSummary.tsx
@@ -1,10 +1,12 @@
 import { AssetIcon } from "@/components/AssetIcon/AssetIcon";
 import { NumberDisplay } from "@/components/NumberDisplay/NumberDisplay";
 import { Block } from "@/components/ui/Block";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/Tooltip/Tooltip";
 import { StkToken } from "@/types/token";
 import { memo } from "react";
 import { InfoIcon } from "lucide-react";
 import { formatUnits } from "viem";
+import { Token } from "@/app/components/Token/Token";
 
 const MAX_TOKENS_TO_SHOW = 4;
 
@@ -30,7 +32,7 @@ export const StakedSummary = memo(({ stkTokens }: StakedSummaryProps) => {
           <NumberDisplay value={totalStakedUSD} type="currency" className="font-bold" />
         </div>
 
-        <div className="flex grow flex-col items-center justify-end sm:flex-row sm:flex-wrap">
+        <div className="flex w-full grow flex-col items-center justify-end sm:flex-row sm:flex-wrap">
           {stkTokens
             // TODO Add dropdown to show more tokens
             .slice(0, MAX_TOKENS_TO_SHOW)
@@ -43,29 +45,66 @@ export const StakedSummary = memo(({ stkTokens }: StakedSummaryProps) => {
                 targetLiquidity,
                 underlying,
                 isUnderlyingStataToken,
+                apyData,
               }) => {
                 const stakedUSD = Number(formatUnits(totalAssets, decimals)) * latestAnswerFormatted;
                 const targetLiquidityUSD = Number(formatUnits(targetLiquidity, decimals)) * latestAnswerFormatted;
 
                 return (
-                  <div
-                    key={address}
-                    className="border-main-100 flex shrink-0 items-center gap-2 border-l px-4 first:border-l-0 last:pr-0"
-                  >
-                    <AssetIcon
-                      type="underlying"
-                      symbol={underlying.symbol}
-                      assetTag={isUnderlyingStataToken ? "stkStata" : "stk"}
-                      className="size-6 shrink-0"
-                    />
+                  <Tooltip key={address}>
+                    <TooltipTrigger asChild>
+                      <div className="group border-main-100 dark:border-main-600 flex shrink-0 cursor-pointer items-center gap-2 border-l px-4 first:border-l-0 last:pr-0">
+                        <AssetIcon
+                          type="underlying"
+                          symbol={underlying.symbol}
+                          assetTag={isUnderlyingStataToken ? "stkStata" : "stk"}
+                          className="size-6 shrink-0"
+                        />
 
-                    <div className="flex items-center gap-1">
-                      <NumberDisplay value={stakedUSD} type="currency" className="text-base font-bold" />
-                      {/* <div className="mt-[1px] text-sm">/</div> */}
-                      {/* <NumberDisplay value={targetLiquidityUSD} type="currency" className="mt-[1px] text-sm" /> */}
-                      <InfoIcon className="text-main-300 ml-0.5 size-4" />
-                    </div>
-                  </div>
+                        <div className="flex items-center gap-1">
+                          <NumberDisplay value={stakedUSD} type="currency" className="text-base font-bold" />
+                          <InfoIcon className="text-main-300 group-hover:text-main-900 ml-0.5 size-4" />
+                        </div>
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom">
+                      <div className="border-main-950 flex min-w-[250px] flex-col gap-1 border bg-white px-4 py-3">
+                        <Token
+                          token={{
+                            address,
+                            symbol: underlying.symbol,
+                            type: isUnderlyingStataToken ? "stkStata" : "stk",
+                          }}
+                        />
+                        <div className="bg-main-100 my-2 h-px w-full" />
+                        <div className="flex items-center justify-between gap-2">
+                          <div>Total Staked</div>
+                          <div className="text-main-900">
+                            <NumberDisplay value={stakedUSD} type="currency" className="font-bold" />
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between gap-2">
+                          <div>Target Liquidity</div>
+                          <div className="text-main-900">
+                            <NumberDisplay value={targetLiquidityUSD} type="currency" className="font-bold" />
+                          </div>
+                        </div>
+                        <div className="bg-main-100 my-2 h-px w-full" />
+                        <div className="flex items-center justify-between gap-2">
+                          <div>Current yield</div>
+                          <div className="text-main-900">
+                            <NumberDisplay value={apyData.rewards.total} type="percent" className="font-bold" />
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between gap-2">
+                          <div>Maximum yield</div>
+                          <div className="text-main-900">
+                            <NumberDisplay value={apyData.rewards.total} type="percent" className="font-bold" />
+                          </div>
+                        </div>
+                      </div>
+                    </TooltipContent>
+                  </Tooltip>
                 );
               },
             )}
